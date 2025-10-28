0dc5c46088e588d9353bf8f4bf27bebcb43015e0
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-10-27 20:22:50 -0600

    Add files via upload

--- product/src/main/java/com/eviasvaldez/product/controller/productApi.java
+++ product/src/main/java/com/eviasvaldez/product/controller/productApi.java
@@ -2,12 +2,10 @@ package com.eviasvaldez.product.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.eviasvaldez.product.entities.Product;
 
 @RestController
